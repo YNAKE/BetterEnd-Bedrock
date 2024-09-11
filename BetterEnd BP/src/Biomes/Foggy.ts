958139ca071e65e63ca75261c4b5b65280e25a44
Goofy9506 <goofy9605@protonmail.com>
2024-09-11 06:15:21 -0500

    feat: added Umbrella Moss small and tall variant

--- BetterEnd BP/src/Biomes/Foggy.ts
+++ BetterEnd BP/src/Biomes/Foggy.ts
@@ -3,3 +3,5 @@ import "./Foggy/MossyGlowShroomCap";
 import "./Foggy/MossyGlowShroomSapling";
 import "./Foggy/CyanMoss";
 import "./Foggy/CreepingMoss";
+import "./Foggy/UmbrellaMoss";
+import "./Foggy/TallUmbrellaMoss";
