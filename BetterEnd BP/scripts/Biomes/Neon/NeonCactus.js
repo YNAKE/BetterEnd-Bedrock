705d93c1b4f7eaf00806aa7bc30a40ecd7a3d559
Halo333X <124174275+Halo333X@users.noreply.github.com>
2024-11-17 18:06:48 -0600

    neon cactus

---
+++ BetterEnd BP/scripts/Biomes/Neon/NeonCactus.js
@@ -0,0 +1,41 @@
+import { BlockPermutation, world } from "@minecraft/server";
+world.beforeEvents.worldInitialize.subscribe((data) => {
+    data.blockComponentRegistry.registerCustomComponent("betterend:neon_cactus", {
+        onTick({ block }) {
+            const sizeState = (block) => block.permutation?.getState("betterend:sizes");
+            const updateState = (block, size, rot) => {
+                block.setPermutation(BlockPermutation.resolve(block.typeId, {
+                    "betterend:sizes": size,
+                    "minecraft:facing_direction": rot,
+                }));
+            };
+            const rot = block.permutation?.getState("minecraft:facing_direction");
+            const sides = [block.north(), block.south(), block.east(), block.west()];
+            const tops = [block.above(), block.below()];
+            // Procesar bloques laterales
+            for (const side of sides) {
+                if (side.typeId !== "betterend:neon_cactus")
+                    continue;
+                const sideState = sizeState(side);
+                if (sideState === 0) {
+                    updateState(block, 1, rot);
+                }
+                else if (sideState === 1) {
+                    updateState(block, 1, rot);
+                }
+            }
+            // Procesar bloques superior e inferior
+            for (const top of tops) {
+                if (top.typeId !== "betterend:neon_cactus")
+                    continue;
+                const topState = sizeState(top);
+                if (topState === 1) {
+                    updateState(block, 2, rot);
+                }
+                else if (topState === 2) {
+                    updateState(block, 2, rot);
+                }
+            }
+        },
+    });
+});
