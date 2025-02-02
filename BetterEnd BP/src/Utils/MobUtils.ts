535eac2b3a7df7953883f827ef493b1f607326b3
Halo333X <124174275+Halo333X@users.noreply.github.com>
2025-02-02 17:25:47 -0600

    MobUtils Script

--- BetterEnd BP/src/Utils/MobUtils.ts
+++ BetterEnd BP/src/Utils/MobUtils.ts
@@ -7,25 +7,25 @@ export default class MobUtils {
   private typeId: string;
   constructor(entity: Entity) {
     this.entity = entity;
-    this.dimension = entity.dimension;
-    this.location = entity.location;
-    this.typeId = entity.typeId;
+    this.dimension = entity?.dimension;
+    this.location = entity?.location;
+    this.typeId = entity?.typeId;
   }
 
   slimeSkin() {
-    if (this.typeId === "minecraft:slime") {
+    if (this?.typeId === "minecraft:slime") {
       const slimeSkins = {
         "betterend:amber_moss": "slime_amber",
         "betterend:chorus_nylium": "slime_chorus",
         "betterend:end_moss_lakes": "slime_megalake",
         "betterend:end_moss": "slime_foggy",
       };
-      const floor = this.dimension.getBlock({ x: this.location.x, y: this.location.y - 1, z: this.location.z })?.typeId;
+      const floor = this?.dimension.getBlock({ x: this.location.x, y: this.location.y - 1, z: this.location.z })?.typeId;
       const moss = Object.keys(slimeSkins).find((key) => key === floor);
       if (!moss) return;
-      if (!this.entity.hasTag("betterend:skin")) {
-        this.entity.addTag("betterend:skin");
-        this.entity.triggerEvent(slimeSkins[moss]);
+      if (!this?.entity.hasTag("betterend:skin")) {
+        this?.entity.addTag("betterend:skin");
+        this?.entity.triggerEvent(slimeSkins[moss]);
       }
     }
   }
