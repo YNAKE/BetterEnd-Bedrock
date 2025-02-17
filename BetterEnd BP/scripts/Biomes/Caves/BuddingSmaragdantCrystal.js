f6a9934783fbcd5dfc383c65b7bfbce3a30d5e55
Halo333X <124174275+Halo333X@users.noreply.github.com>
2025-02-17 12:30:16 -0600

    Budding Smaragdant Crystal (Crystal Shard Generator) Script

---
+++ BetterEnd BP/scripts/Biomes/Caves/BuddingSmaragdantCrystal.js
@@ -0,0 +1,40 @@
+import { world, Direction, BlockPermutation } from "@minecraft/server";
+world.beforeEvents.worldInitialize.subscribe(data => {
+    data.blockComponentRegistry.registerCustomComponent('betterend:budding_smaragdant_crystal', {
+        onTick({ block }) {
+            const { location: loc, dimension: dim } = block;
+            // Ticks [ 20000, 40000 ] [16 min a 33 min]
+            const sides = [
+                {
+                    direction: Direction.Down,
+                    vector: { x: loc.x, y: loc.y - 1, z: loc.z }
+                },
+                {
+                    direction: Direction.East,
+                    vector: { x: loc.x + 1, y: loc.y, z: loc.z }
+                },
+                {
+                    direction: Direction.North,
+                    vector: { x: loc.x, y: loc.y, z: loc.z + 1 }
+                },
+                {
+                    direction: Direction.South,
+                    vector: { x: loc.x, y: loc.y, z: loc.z - 1 }
+                },
+                {
+                    direction: Direction.Up,
+                    vector: { x: loc.x, y: loc.y + 1, z: loc.z }
+                },
+                {
+                    direction: Direction.West,
+                    vector: { x: loc.x - 1, y: loc.y, z: loc.z }
+                }
+            ];
+            const randomSide = sides[Math.floor(Math.random() * sides.length)];
+            const perm = BlockPermutation.resolve(block.typeId + '_shard', {
+                "minecraft:facing_direction": randomSide.direction.toLocaleLowerCase()
+            });
+            dim.setBlockPermutation(randomSide.vector, perm);
+        }
+    });
+});
