b84fa9e387cd8788dd76b99b000ec3a4d7a3a360
Halo333X <124174275+Halo333X@users.noreply.github.com>
2025-02-24 18:27:42 -0600

    Magnula and Rubinea Vine Missing

---
+++ BetterEnd BP/scripts/Biomes/Caves/Magnula.js
@@ -0,0 +1,31 @@
+import { world } from "@minecraft/server";
+import PlantUtils from "Biomes/PlantsUtils";
+world.beforeEvents.worldInitialize.subscribe(data => {
+    data.blockComponentRegistry.registerCustomComponent('betterend:magnula', {
+        onPlayerDestroy({ block, player }) {
+            const plantUtils = new PlantUtils(block, player);
+            plantUtils.onBreak('betterend:magnulasss');
+        },
+        onTick({ block }) {
+            const tenaneaFlowers = new TenaneaFlowers(block);
+            tenaneaFlowers.setTexture();
+        }
+    });
+});
+class TenaneaFlowers {
+    constructor(block) {
+        this.block = block;
+        this.middle = block.isAir ? null : block.permutation.withState('betterend:top', false);
+        this.top = block.isAir ? null : block.permutation.withState('betterend:top', true);
+    }
+    setTexture() {
+        const block = this.block;
+        const { dimension, location } = block;
+        const down = { x: location.x, y: location.y - 1, z: location.z };
+        const blockDown = dimension.getBlock(down);
+        if (blockDown.isAir)
+            this.top ? block.setPermutation(this?.top) : null;
+        else
+            this.middle ? block.setPermutation(this?.middle) : null;
+    }
+}
--- BetterEnd BP/src/Biomes/Caves.ts
+++ BetterEnd BP/src/Biomes/Caves.ts
@@ -1,3 +1,5 @@
 import "./Caves/PlantIndex";
 import "./Caves/CaveBushLeaves";
 import "./Caves/BuddingSmaragdantCrystal";
+import "./Caves/Magnula";
+import "./Caves/Rubinea";
